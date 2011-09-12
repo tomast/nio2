b160b296081986a787923fc0ba4289689edab629
Tomas Turek <turek.tomas@gmail.com>
2011-09-12 21:29:56 +0200

    Added FileVisitor implementation and created test for printing all files info

---
+++ src/main/java/com/tieto/nio2/walkfiletree/PrintXmlFileInformation.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tieto.nio2.walkfiletree;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ *
+ * @author tur075
+ */
+public class PrintXmlFileInformation implements FileVisitor<Path> {
+
+    private final static String XML_FILE_PATTERN = "glob:*.xml";
+    private PathMatcher pathMatcher;
+
+    public PrintXmlFileInformation() {
+        pathMatcher = FileSystems.getDefault().getPathMatcher(XML_FILE_PATTERN);
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        System.out.format("Started listing directory: %s%n", dir);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (pathMatcher.matches(file.getFileName())) {
+            if (attrs.isSymbolicLink()) {
+                System.out.format("Symbolic link: %s ", file);
+            } else if (attrs.isRegularFile()) {
+                System.out.format("Regular file: %s ", file);
+            } else {
+                System.out.format("Other: %s ", file);
+            }
+            System.out.println("(" + attrs.size() + "bytes)");
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.err.println(exc);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.format("Finished listing directory: %s%n", dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
---
+++ src/test/java/com/tieto/nio2/walkfiletree/PrintXmlFileInformationTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tieto.nio2.walkfiletree;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tur075
+ */
+public class PrintXmlFileInformationTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of preVisitDirectory method, of class PrintFileInformation.
+     */
+    @Test
+    public void testWalkFileTree() throws Exception {
+        final Path rootDir = Paths.get(getClass().getClassLoader().getResource("watchingFolder").toURI());
+        assertNotNull(rootDir);
+        
+        Files.walkFileTree(rootDir, new PrintXmlFileInformation());
+    }
+
+}

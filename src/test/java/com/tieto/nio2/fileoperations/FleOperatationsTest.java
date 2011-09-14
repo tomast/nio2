ea4de1231bc8f68addf13266dfc5e3a86bcf7b44
Tomas Turek <turek.tomas@gmail.com>
2011-09-14 09:33:59 +0200

    Added more file operations examples

--- src/test/java/com/tieto/nio2/fileoperations/FleOperatationsTest.java
+++ src/test/java/com/tieto/nio2/fileoperations/FleOperatationsTest.java
@@ -3,33 +3,30 @@
  */
 package com.tieto.nio2.fileoperations;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
-import org.junit.After;
-import org.junit.Before;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 
 /**
- * @author tur075
+ * @author tureltom
+ *
  */
 public class FleOperatationsTest {
-    
-    @Before
-    public void setUp() throws Exception {
-    }
-    
-    @After
-    public void tearDown() throws Exception {
-    }
     
     @Test
     public void testDeleteNotExistingFileJava6() {
@@ -45,6 +42,29 @@ public class FleOperatationsTest {
         final Path testFile = Paths.get("watchingFolder/test.txt");
         assertNotNull(testFile);
         Files.delete(testFile);
+    }
+    
+    @Test (expected = DirectoryNotEmptyException.class)
+    public void testDeleteNotEmptyDirectory() throws URISyntaxException, IOException{
+        final Path p1 = Paths.get(getClass().getClassLoader().getResource("watchingFolder").toURI());
+        Files.delete(p1);
+    }
+    
+    @Test
+    public void testResolvePath() throws URISyntaxException{
+        final Path p1 = Paths.get(getClass().getClassLoader().getResource("watchingFolder").toURI());
+        final Path p2 = Paths.get("src");
+        
+        System.out.println(p1.resolve(p2));
+    }
+    
+    @Test
+    public void testRelativizePath() throws URISyntaxException{
+        final Path p1 = Paths.get("c:/");
+        final Path p2 = Paths.get(getClass().getClassLoader().getResource("watchingFolder").toURI());
+        
+        System.out.println(p1.relativize(p2));
+        System.out.println(p2.relativize(p1));
     }
     
     @Test
